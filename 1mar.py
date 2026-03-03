bc871b95471278bcc0a1be6636d952e9c255787f
Pranali Sali <pranali23102003@gmail.com>
2026-03-03 22:39:23 +0530

    Add files via upload

---
+++ 1mar.py
@@ -0,0 +1,54 @@
+# 1: frozen set:
+#create a frozen set:
+# s = {1,2,3,4,5}
+# s.add(90)
+
+# fs = frozenset(s)
+# print(type(fs))
+
+# 2: Range data type:
+# r = range(5)
+# print(type(r))
+
+
+
+# r = range(5)
+# print(r)
+# s = ""
+# N = 9
+# for i in r(N):
+#     print(i)
+
+# r = range(5)
+# print(r)
+# s = "INSTAGRAM"
+# N = len(s)
+# for i in range (len(s)):
+#     print(i, "------>",s[i])
+
+# s = "INSTAGRAM"
+
+# for i in range(len(s)-1, -1, -1):
+#     print(i, "------>", s[i])
+
+
+# for i in range(len(s)):
+#     print(8-i, "--->",s[8-i])
+
+# Homework:
+# Iterate a List using range() 
+# my_list = [10,20,30,40]
+# for i in range(len(my_list)):
+#     print("Index:", i, "value:", my_list[i])
+
+
+# Iterate a Tuple using range()
+# my_tuple = [1,2,3,4]
+# for i in range(len(my_tuple)):
+#     print("Index:", i, "value:", my_tuple[i])
+
+
+# Iterate a String using range()
+my_string = "Pranali"
+for i in range(len(my_string)):
+    print("Index", i, "character", my_string[i])
